ab900e597ebf98318f2532cee703f47c5be29da2
Vallas25 <jansensmaurits@gmail.com>
2026-04-25 13:25:31 +0200

    fixed movement

--- player.py
+++ player.py
@@ -13,6 +13,7 @@ class Player(CircleShape):
         self.lives = LIVES
         self.org_ship = pygame.image.load("assets/asteroid_ship.png").convert_alpha()
         self.ship = pygame.transform.rotozoom(self.org_ship, self.rotation, 2)
+        self.rect = self.ship.get_rect(center = self.position)
     
     # in the Player class
     
@@ -20,11 +21,12 @@ class Player(CircleShape):
         pass
 
     def draw(self, screen):
-       screen.blit(self.ship, self.position)
+       screen.blit(self.ship, self.rect)
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
         self.ship = pygame.transform.rotozoom(self.org_ship, self.rotation, 2)
+        self.rect = self.ship.get_rect(center=self.position)
     
     def update(self, dt):
         self.cooldown_timer -= dt
@@ -35,13 +37,13 @@ class Player(CircleShape):
             self.rotate(dt = dt)
 
         if keys[pygame.K_d]:
-            self.rotate(dt = -1 * dt)
+            self.rotate(dt = -dt)
         
         if keys[pygame.K_w]:
-            self.move(dt = -1 * dt)
+            self.move(dt = dt)
         
         if keys[pygame.K_s]:
-            self.move(dt = dt)
+            self.move(dt = -dt)
         
         if keys[pygame.K_SPACE]:
             if self.cooldown_timer > 0:
@@ -52,12 +54,13 @@ class Player(CircleShape):
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, -1)
-        direction = unit_vector.rotate(self.rotation)
+        direction = unit_vector.rotate(-self.rotation)
         self.position += direction * PLAYER_SPEED * dt
+        self.rect.center = self.position
 
     def shoot(self):
         shot = Shot(self.position[0], self.position[1])
-        shot.velocity = (pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED)
+        shot.velocity = (pygame.Vector2(0,-1).rotate(-self.rotation) * PLAYER_SHOOT_SPEED)
     
     def score_increase(self):
         self.score += 1
